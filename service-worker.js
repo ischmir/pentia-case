d5170b6cf8b8899969142f7ef51fc700e0892332
StikkelMaal <Mikkel.Staal@outlook.dk>
2025-02-26 13:45:13 +0100

    update

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,42 @@
+const CACHE_NAME = "pwa-cache-v1";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/css/styles.css",
+    "/js/app.js",
+    "/assets/icon-192x192.png",
+    "/assets/icon-512x512.png"
+];
+
 self.addEventListener("install", (event) => {
-    console.log("Service Worker installing.");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    console.log("Service Worker Installed");
 });
 
 self.addEventListener("activate", (event) => {
-    console.log("Service Worker activated.");
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log("Old cache deleted");
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    console.log("Service Worker Activated");
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
